2c98c0264052728033939b782aa173847c5b3046
Ahmad <ahmadalhatlan@gmail.com>
2025-10-26 16:05:13 +0300

    fix error

--- terraform/modules/KeyVault/main.tf
+++ terraform/modules/KeyVault/main.tf
@@ -39,3 +39,18 @@ resource "azurerm_key_vault_access_policy" "aks" {
     "List"
   ]
 }
+
+# Access policy for the Terraform service principal (current identity)
+resource "azurerm_key_vault_access_policy" "current" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Purge"
+  ]
+}
